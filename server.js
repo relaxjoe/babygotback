5323c323ae5990e051bef895e36ed451b9abfa9b
Joe <joeyndiebel@gmail.com>
2024-08-04 08:21:50 -0500

    front end

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const routes = require('./routes');
 const sequelize = require('./config/connection'); // Import sequelize connection module
+const path = require('path');
+
+
 // import sequelize connection
 
 const app = express();
@@ -10,6 +13,10 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(routes);
+app.use(express.static(path.join(__dirname, 'public')));
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
